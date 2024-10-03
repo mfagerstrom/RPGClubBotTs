2b8aa50b18b53e54ce4fb8db7f037dd6254c0f3d
Mike Fagerstrom <mfagerstrom@globalspec.com>
2024-10-02 21:06:44 -0400

    chore: removing wrong tsc

---
+++ src/config/database.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import * as db from 'mssql';
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  server: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  options: {
+    encrypt: false,
+    trustServerCertificate: false,
+  },
+};
+
+const connectionString = `Server=${config.server},${config.port};Database=${config.database};User Id=${config.user};Password=${config.password};Encrypt=false`;
+
+export const database = new db.ConnectionPool(connectionString);
