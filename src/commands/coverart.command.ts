0e015a4f576c8186950c8781090b7f307196670c
Mike Fagerstrom <mfagerstrom@globalspec.com>
2024-10-13 12:40:22 -0400

    feat: implemented /coverart command

---
+++ src/commands/coverart.command.ts
@@ -0,0 +1,25 @@
+import type { CommandInteraction } from "discord.js";
+import { ApplicationCommandOptionType } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+import { EmbedBuilder } from "discord.js";
+import { searchHltb } from "../functions/SearchHltb.js";
+
+@Discord()
+export class Coverart {
+  @Slash({ description: "Video Game Cover Art search, courtesy of Google and HLTB" })
+  async coverart(
+    @SlashOption({
+      description: "Enter game title and optional descriptors (we're googling!)",
+      name: "title",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    title: string,
+    interaction: CommandInteraction,
+  ): Promise<void> {
+    const result = await searchHltb(title)
+    await interaction.reply({
+        content: result.imageUrl,
+      });
+  }
+}
